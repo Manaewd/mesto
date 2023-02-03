aed4ad7328729edba1422c8cb9d345b6f8c828cd
Denis Manaev <manaewd@yandex.ru>
2023-02-04 04:04:00 +0700

    feat: add

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,61 @@
+//попапы
+const popupEditProfile = document.querySelector('.popup_edit_profile');
+const popupAddProfile = document.querySelector('.popup_add-profile');
+
+//btn открыть
+const popupOpnBtnElement = document.querySelector('.profile__edit-button');
+const popupAddButton = document.querySelector('.profile__add-button');
+//btn закрыть
+const popupCloseButton= document.querySelectorAll('.popup__close');
+//form
+const popupEditForm = document.querySelector('.popup__content');
+const popupAddForm = document.querySelector('.popup__add-card');
+//редактировать профиль
+const profileTitle = document.querySelector('.profile__title'); 
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const popupTitle = document.querySelector('.popup__item_type_name'); 
+const popupSubtitle = document.querySelector('.popup__item_type_job'); 
+//добавить element
+const popupAddName = document.querySelector('.popup__item_add_name'); 
+const popupAddUrl = document.querySelector('.popup__item_add_url');
+
+const popupAddImage = document.querySelector('.popup_add-image');
+const popupImage = document.querySelector('.popup__image');
+const popupImageName = document.querySelector('popup__image-name');
+
+
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
+
+popupOpnBtnElement.addEventListener('click', function openEditPopup() {
+  openPopup(popupEditProfile);
+  popupTitle.value = profileTitle.textContent;
+  popupSubtitle.value = profileSubtitle.textContent;
+});
+
+popupAddButton.addEventListener('click', function () {
+  openPopup(popupAddProfile);
+});
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+popupCloseButton.forEach(function (button) {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', function () { closePopup(popup) });
+});
+
+popupEditForm.addEventListener('submit', function submit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popupTitle.value;
+  profileSubtitle.textContent = popupSubtitle.value;
+  
+  closePopup(popupEditProfile)
+});
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -25,51 +83,45 @@ const initialCards = [
   }
 ]; 
 
-// добавление карточек при загрузки
-const cardElement = document.querySelector('.elements');
-const template = document.querySelector('#template').content;
+const mestoElements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#template').content;
 
-initialCards.forEach(function (element) {
-    const task = template.cloneNode(true);
+function mestoElement(name, link) {
 
-    task.querySelector('.element__title').textContent = element.name;
-    task.querySelector('.element__image').alt = element.name;
-    task.querySelector('.element__image').src = element.link;
+  const mestoElement = elementTemplate.cloneNode(true);
+  mestoElement.querySelector('.element__title').textContent = name;
+  mestoElement.querySelector('.element__image').alt = name;
+  mestoElement.querySelector('.element__image').src = link;
 
-    cardElement.append(task);
+  const trashButton = mestoElement.querySelector('.element__trash-button');
+  trashButton.addEventListener('click', function (evt) {
+    evt.target.closest('.element').remove();
+  })
+
+  const elementButton = mestoElement.querySelector('.element__button');
+  elementButton.addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__button_type_active');
+  })
+
+  const elementImage = mestoElement.querySelector('.element__image');
+
+  elementImage.addEventListener('click', function () {
+    openPopup(popupAddImage);
+    popupImage.src = link;
+    popupImageName.textContent = name;
+  })
+
+  return mestoElement;
+}
+
+initialCards.forEach(function (ele) {
+  mestoElements.append(mestoElement(ele.name, ele.link));
 });
 
-// popup_edit_profile + popup_add-profile
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupContent = document.querySelector('.popup__content');
-const popupName = popupContent.querySelector('.popup__item_type_name');
-const popupJob = popupContent.querySelector('.popup__item_type_job');
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
+popupAddForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  mestoElements.prepend(mestoElement(popupAddName.value, popupAddUrl.value));
 
-const popupOpenAddProfile = document.querySelector('.profile__add-button');
-
-const openPopup = function() {
-    popupElement.classList.add('popup_opened');
-    popupName.value = profileTitle.textContent;
-    popupJob.value = profileSubtitle.textContent;
-}
-const closePopup = function() {
-    popupElement.classList.remove('popup_opened');
-}
-
-function handleFormSubmit(evt) {
-    evt.preventDefault();
-    profileTitle.textContent = popupName.value;
-    profileSubtitle.textContent = popupJob.value;
-    closePopup();
-}
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupContent.addEventListener('submit', handleFormSubmit);
-popupOpenAddProfile.addEventListener('click', openPopup);
-
-
-
+  evt.target.reset();
+  closePopup(popupAddProfile);
+})
