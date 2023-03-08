bcf6258af065fb0ffdb5545a02d8aa16d8940c68
Denis Manaev <manaewd@yandex.ru>
2023-03-09 00:12:35 +0700

    refactor:class

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
+import Card from './Card.js';
+import FormValidator from './FormValidator.js';
+import { initialCards, formValidationConfig } from './validate.js'
+
 //попапы
 const profilePopup = document.querySelector('.popup_edit_profile');
 const cardPopup = document.querySelector('.popup_add-profile');
 const popups = document.querySelectorAll('.popup');
-//bbuttons
+//buttons
 const profileEditOpen = document.querySelector('.profile__edit-button');
 const submitButtonSelector = document.querySelector('.popup__button-save')
 const closeButtons = document.querySelectorAll('.popup__button-close');
@@ -22,6 +26,7 @@ const popupAddImage = document.querySelector('.popup_add-image');
 const popupFullScreenImage = document.querySelector('.popup__image');
 const popupDescription = document.querySelector('.popup__image-name');
 
+//закрыть попап по Esc
 function closeWithEscape(evt) {
     if (evt.key === 'Escape') {
         const popup = document.querySelector('.popup_opened');
@@ -36,18 +41,29 @@ popups.forEach((popups) => {
     });
 });
 
+//открыть попап
 function openPopup(popup) {
     popup.classList.add('popup_opened');
     document.addEventListener('keydown', closeWithEscape);
 };
+
+//закрыть попап
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeWithEscape)
+};
+
+
 profileEditOpen.addEventListener('click', function openEditPopup() {
     openPopup(profilePopup);
     popupTitle.value = profileTitle.textContent;
     popupSubtitle.value = profileSubtitle.textContent;
 });
+
 profileAddImageOpen.addEventListener('click', function() {
     openPopup(cardPopup);
 });
+
 profileAddImageOpen.addEventListener('click', function addFormSubmit() {
     openPopup(cardPopup);
     const submitButtonSelector = cardPopup.querySelector('.popup__button-save')
@@ -55,41 +71,22 @@ profileAddImageOpen.addEventListener('click', function addFormSubmit() {
     submitButtonSelector.classList.add('popup__button-disabled');
 })
 
-function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', closeWithEscape)
-};
+
 closeButtons.forEach(function(button) {
     const popup = button.closest('.popup');
     button.addEventListener('click', function() {
         closePopup(popup)
     });
 });
+
 popupEditForm.addEventListener('submit', function submit(evt) {
     evt.preventDefault();
     profileTitle.textContent = popupTitle.value;
     profileSubtitle.textContent = popupSubtitle.value;
     closePopup(profilePopup)
 });
-const initialCards = [{
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-}, {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-}, {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-}, {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-}, {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-}, {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-}];
+
+
 const mestoElements = document.querySelector('.elements');
 const elementTemplate = document.querySelector('#template').content;
 
@@ -99,13 +96,16 @@ function mestoElement(name, link) {
     mestoElement.querySelector('.element__image').src = link;
     mestoElement.querySelector('.element__image').setAttribute('alt', 'Фотография ' + name);
     const trashButton = mestoElement.querySelector('.element__trash-button');
+    
     trashButton.addEventListener('click', function(evt) {
         evt.target.closest('.element').remove();
     })
+    
     const elementButton = mestoElement.querySelector('.element__button');
     elementButton.addEventListener('click', function(evt) {
         evt.target.classList.toggle('element__button_type_active');
     })
+    
     const elementImage = mestoElement.querySelector('.element__image');
     elementImage.addEventListener('click', function() {
         openPopup(popupAddImage);
@@ -115,12 +115,20 @@ function mestoElement(name, link) {
     })
     return mestoElement;
 }
+
 initialCards.forEach(function(ele) {
     mestoElements.append(mestoElement(ele.name, ele.link));
 });
+
 popupAddForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
     mestoElements.prepend(mestoElement(popupAddName.value, popupAddUrl.value));
     evt.target.reset();
     closePopup(cardPopup);
-})
+});
+
+const popupFormEditValidator = new FormValidator(formValidationConfig, popupEditForm);
+popupFormEditValidator.enableValidation();
+
+const popupFormAddValidator = new FormValidator(formValidationConfig, popupAddForm);
+popupFormAddValidator.enableValidation();
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,53 +1,27 @@
-const formValidationConfig = {
+export const formValidationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__item',
     errorClass: 'popup__item_type_error',
     submitButtonSelector: '.popup__button-save',
     inactiveButtonClass: 'popup__button-disabled',
-}
+};
 
-function disableSubmit(evt) {
-    evt.preventDefault();
-}
-
-function enableValidation(config) {
-    const formList = Array.from(document.querySelectorAll(config.formSelector));
-    formList.forEach((form) => {
-        form.addEventListener('submit', disableSubmit);
-        form.addEventListener('input', () => {
-            toggleButton(form, config);
-        });
-        addInputListners(form, config);
-        toggleButton(form, config);
-    });
-}
-
-function handleFormInput(evt, config) {
-    const input = evt.target;
-    const inputId = input.id;
-    const errorElement = document.querySelector(`#${inputId}-error`);
-    if (input.validity.valid) {
-        input.classList.remove(config.errorClass)
-        errorElement.textContent = '';
-    } else {
-        input.classList.add(config.errorClass);
-        errorElement.textContent = input.validationMessage;
-    }
-}
-
-function toggleButton(form, config) {
-    const buttonSubmit = form.querySelector(config.submitButtonSelector);
-    const isValidForm = form.checkValidity();
-    buttonSubmit.disabled = !isValidForm;
-    buttonSubmit.classList.toggle(config.inactiveButtonClass, !isValidForm);
-}
-
-function addInputListners(form, config) {
-    const inputList = form.querySelectorAll(config.inputSelector);
-    inputList.forEach(function(item) {
-        item.addEventListener('input', (evt) => {
-            handleFormInput(evt, config)
-        });
-    });
-}
-enableValidation(formValidationConfig);
+export const initialCards = [{
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+}, {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+}, {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+}, {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+}, {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+}, {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+}];
